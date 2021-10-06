3e10467cf10f4240dbf1ad324863276a5c6a6963
iceweasel <johnsjosephk@protonmail.com>
2021-10-06 15:17:07 +0530

    computed methods

---
+++ directive and computed/script.js
@@ -0,0 +1,41 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+             firstName: 'Baba' , 
+             lastName: 'Yaga' , 
+            } 
+        } , 
+
+    methods: {
+        fullName(){
+            return `${this.firstName} ${this.lastName} ` ; 
+        } ,
+
+    }
+}).mount('#app')  ;
+
+// computed methods
+
+const app = Vue.createApp({
+   data() {
+       return {  
+            author :{
+             name : 'Boba Fett' , 
+             books : [
+                 'Vue For dummies ' , 
+                 'Vue mastery ' ,
+                 'Vue API References ' , 
+             ] ,
+            }
+     }
+   } , 
+
+   computed: {
+       // a computed getter 
+       publishedBooks() {
+        return this.author.books.length > 0 ? 'Sì' : 'Nunca' ; 
+       }
+   }
+}).mount('#computed'); 
+
+//
