f8a681960d709d0dccbd15c49fe2daa403db1698
iceweasel <johnsjosephk@protonmail.com>
2021-10-11 09:42:29 +0530

    Time task Ui complete

---
+++ starter/src/components/Completed.vue
@@ -0,0 +1,45 @@
+<template>
+        
+        <div class="table-container">
+         <table border="1">
+        <tr>
+            <th>Task#ID</th>
+            <th>Task Name</th>
+            <th  >Description</th>
+            <th >Start Date</th>
+            <th >End Date</th>
+            <th >Completed on</th>
+            <th >Assignees</th>
+        </tr>
+        <tr style="height:50px;">
+            <td width="70px" >1000</td>
+            <td  > bebeb</td>
+            <td rowspan="4" style="word-wrap:break-word;overflow-y:scroll;" width="350px" valign="top">Random Description goes 
+                here . 
+            </td>
+            <td width="95px"  >22-09-1991</td>
+            <td width="95px"  >16-12-1991</td>
+            <td width="120px"  >16-12-1991</td>
+            <td rowspan="4" style="word-wrap:break-word;overflow-y:scroll;" width="200px" valign="top">francesco pagliei </td>
+        </tr>
+         </table>
+    </div>
+   
+     
+
+ </template>
+
+ <script>
+  export default{
+      name: 'Completed' , 
+      data() {
+          return{
+
+          }
+      }
+  }
+ </script>
+ 
+<style scoped>
+
+</style>
---
+++ starter/src/components/Search.vue
@@ -0,0 +1,63 @@
+ <template>
+     <h1>Time Task Clone </h1>
+    <div id="search-bar" class="search">
+        
+        <select name="cars" id="cars">
+        <option value="select"></option>
+        <option value="taskid">taskid</option>
+        <option value="taskname">Mercedes</option>
+        <option value="person">Audi</option>
+        </select>
+
+        <input  placeholder="Search with taskid , taskname or person " />
+    </div>
+
+    <div class="button-container">
+        <button href="#" class="tt-atom-btn  add">Add Task</button>
+        <button href="#"  class="tt-atom-btn history">Task History</button>
+    </div>
+     
+    <h2>Ongoing Tasks</h2>  
+
+ </template>
+
+
+ <script>
+   export default{
+    name: 'Search', 
+        data(){
+            return {
+
+            }
+        }
+   };
+ </script>
+ 
+ <style scoped>
+    .search{
+        display: flex;
+        flex-direction: row;
+        margin:10px ;
+    }
+
+    input{
+        width:30vw ; 
+    }
+
+    .button-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+     }
+
+    .button-container > button {
+        padding: 10px;
+        margin: 10px; 
+    }
+  
+  .history{
+      background-color: #ba4978;
+  }
+
+
+ </style>
