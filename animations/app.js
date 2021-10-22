74b7a2017c82bcd83f3a532b6e75a21c08ab4f56
iceweasel <johnsjosephk@protonmail.com>
2021-10-22 17:19:33 +0530

    animations vis JS and hooks

--- animations/app.js
+++ animations/app.js
@@ -2,9 +2,28 @@
 const Demo = {
   data() {
     return {
-      noActivated: false
+      noActivated: false ,
+      x : 0 ,
     }
-  }
+  } ,
+
+  methods: {
+    focusInput() {
+      this.$refs.input.focus()
+    } ,
+
+    xCoordinate(e) {
+      this.x = e.clientX
+    },
+
+
+  },
+  
 }
 
+ 
+ 
+
+
 Vue.createApp(Demo).mount('#demo')
+
--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,4 +1,5 @@
 <template> 
+   <button type="button" @click="flag = !flag" >TOGGLE </button>
     <!-- <app-form>
        <template v-slot:help>
           <p>Need some Help here ? </p>
@@ -32,7 +33,7 @@
     </div>
     <button @click="changeName">Change Name</button> -->
 
-    <select v-model="optionName">
+    <!-- <select v-model="optionName">
         <option value="Home"> HOME </option>
         <option value="About"> ABOUT </option>
         <option value="Contact"> CONTACT US </option>
@@ -40,30 +41,49 @@
 
     <keep-alive>
        <component :is="optionName" ></component>
-    </keep-alive>
+    </keep-alive> -->
+
+
+   <!-- Animation -->
+    <!-- <transition name="zoom" type="animation" appear>
+      <h2 v-if="flag" key="t1">hey Y'all </h2>
+    </transition> -->
+ 
+   <!-- Animation with  JS -->
+    <transition 
+      name="zoom"
+      type="animation"
+     @before-enter="beforeEnter"
+     @enter="enter"
+     @after-enter="afterEnter"
+          @before-leave="beforeLeave"
+     @leave="leave"
+     @after-leave="afterLeave"
+     appear>
+      <h2 v-if="flag" >hey Y'all </h2>
+    </transition>
+ 
 
 </template>
 
 <script>
 // import AppForm from './components/Form.vue' 
  //import List from './components/List.vue' 
- import  Home from './components/Home.vue' 
- import  About from './components/About.vue' 
- import  Contact from './components/Contact.vue' 
+// import  Home from './components/Home.vue' 
+// import  About from './components/About.vue' 
+ //import  Contact from './components/Contact.vue' 
 
 export default {
   name: 'App',
   components: {
     
-      Home , 
-      About , 
-      Contact , 
   } ,
   data() {
     return {
        bucketList: ['Travel Abroad' , 'Write Everyday' , 'Cook a meal atleast everyday' , 'Clean room once a week'] ,
        user : 'Francesco' , 
        optionName : 'Home' , 
+       flag: true  , 
     }
   },
 
@@ -79,7 +99,33 @@ export default {
         this.user='Boba Fett'
     },
 
+   beforeEnter(el){
+      console.log('before-enter hook activated' ,el );
+   }, 
 
+   enter(el,done){
+      console.log('enter hook activated ' ,el );
+      done();
+   }, 
+
+   afterEnter(el){
+      console.log('after-enter hook activated' ,el );      
+   }, 
+
+   beforeLeave(el){
+      console.log('before-leave hook activated' ,el );      
+   }, 
+
+   leave(el, done){
+      console.log('Leave hook activated' ,el );            
+      done();
+   }, 
+
+   afterLeave(el){
+      console.log('after-leave hook activated' ,el );      
+   }, 
+
+ 
   } ,
 
   computed : {
@@ -96,4 +142,48 @@ export default {
    display: block ; 
    margin-top: 40px ; 
  }
+
+h2{
+   width: 400px ; 
+   padding: 20px ;
+   margin :20px ;
+}
+
+@keyframes zoom-in {
+   from {
+       transform: scale(0,0);
+
+   }
+   to {
+             transform: scale(1,1);
+   }
+}
+
+@keyframes zoom-out {
+   from {
+      transform: scale(1,1);
+   }
+   to {
+         transform: scale(0,0);
+   }
+}
+
+.zoom-enter-active{
+animation: zoom-in 1s linear forwards;
+transition: all 3s linear;
+}
+
+.zoom-leave-active{
+      animation: zoom-out 1s linear forwards;
+   transition: all 1s linear;
+}
+
+.zoom-enter-from{
+   opacity: 0;
+}
+
+.zoom-leave-to{
+   opacity: 0;
+}
+
  </style>
